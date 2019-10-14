ccbf7c0e3523978197297cc57a5460bd4afaf2f6
mono1140 <11potato30@gmail.com>
2019-10-14 22:57:25 +0900

    :art: Scene.cpp Scene.h のcodeの追加

--- ActionPractice/ActionPractice/Scene.cpp
+++ ActionPractice/ActionPractice/Scene.cpp
@@ -1,1 +1,72 @@
-﻿
+﻿#include <stdio.h>
+#include <windows.h>
+#include <d3d9.h>
+
+#include "Engine.h"
+
+#include "Scene.h"
+
+//Pointa point;
+
+SceneId Scene::GetCurrentSceneId()
+{
+	return g_CurrentSceneId;
+}
+
+SceneStep Scene::GetCurrentSceneStep()
+{
+	return g_CurrentSceneStep;
+}
+
+void Scene::ChangeSceneStep(SceneStep next_step)
+{
+	g_CurrentSceneStep = next_step;
+}
+
+
+void Scene::UpdateScene(DirectX* directX,Engine engine,Device device)
+{
+	int current_scene_id = SceneId::TitleScene;
+
+
+	engine.UpdateInput(directX,device);
+	SceneId result_id = g_CurrentSceneId;
+
+	switch (g_CurrentSceneId)
+	{
+		// タイトルシーン
+	case SceneId::TitleScene:
+		//result_id = TitleSceneMain(directX, count, flamCount);
+		break;
+	}
+
+	DrawScene(directX,engine);
+
+	if (result_id != g_CurrentSceneId)
+	{
+		g_CurrentSceneId = result_id;
+		ChangeSceneStep(SceneStep::InitStep);
+	}
+
+}
+
+void Scene::DrawScene(DirectX* directX,Engine engine)
+{
+	if (engine.DrawStart(directX) == false)
+	{
+		return;
+	}
+	if (g_CurrentSceneStep == SceneStep::MainStep)
+	{
+		switch (g_CurrentSceneId)
+		{
+		case SceneId::TitleScene:
+			//DrawTitleScene(directX, count);
+			break;
+		}
+	}
+
+
+	directX->pDevice->EndScene();
+	directX->pDevice->Present(NULL, NULL, NULL, NULL);
+}
--- ActionPractice/ActionPractice/Scene.h
+++ ActionPractice/ActionPractice/Scene.h
@@ -1,1 +1,58 @@
-﻿
+﻿#ifndef SCENE_H_
+#define SCENE_H_
+
+#include"Graphic.h"
+#include"DirectX.h"
+#include"Engine.h"
+
+// Step 数
+enum SceneStep
+{
+	InitStep,			//!< 初期化
+	MainStep,			//!< メイン
+	EndStep,			//!< 終了
+};
+
+// SceneId の数
+enum SceneId
+{
+	TitleScene,			//!< タイトルシーン
+	ResultSceneTwo,		//!< 難易度設定シーン
+	HelpScene,          //!< ヘルプ(遊戯説明画面)シーン
+	GameScene,			//!< ゲーム本編シーン
+	ResultScene,		//!< リザルトシーン
+	SceneIdMax,			//!< シーンID最大
+};
+
+
+class Scene
+{
+private:
+
+	SceneId g_CurrentSceneId = SceneId::TitleScene;		// 動作中シーンID
+	SceneStep g_CurrentSceneStep = SceneStep::InitStep;	// 動作中シーンのステップ
+
+public:
+
+	void ChangeSceneStep(SceneStep next_step);
+
+	void UpdateScene(DirectX* directX,Engine engine,Device device);
+
+	void DrawScene(DirectX* directX,Engine engine);
+
+	/**
+	* @brief 現在のシーンIDを戻り値で取得する
+	* @return 現在のシーンID
+	*/
+	SceneId GetCurrentSceneId();
+
+	/**
+	* @brief 現在のステップを戻り値で取得する
+	* @return 現在のステップ
+	*/
+	SceneStep GetCurrentSceneStep();
+
+};
+
+
+#endif // !
