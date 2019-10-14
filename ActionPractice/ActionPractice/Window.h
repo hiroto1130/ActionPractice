9ead2d88ea084a8c2abaf587206707f7e73751d6
mono1140 <11potato30@gmail.com>
2019-10-14 22:06:44 +0900

    :art: Window.cpp Window.h のcode追加

--- ActionPractice/ActionPractice/Window.h
+++ ActionPractice/ActionPractice/Window.h
@@ -1,1 +1,17 @@
-﻿
+﻿#ifndef WINDOW_H
+#define WINDOW_H
+
+#include"DirectX.h"
+
+class Window
+{
+public:
+
+	// LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+	HWND MakeWindow(HWND*, HINSTANCE, int, int);
+	HWND MakeFullWindow(HINSTANCE, int, int);
+
+
+};
+
+#endif
