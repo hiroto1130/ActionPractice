13ffb34155ae663951a7d4c0c7f09620e5c5cf8a
mono1140 <11potato30@gmail.com>
2019-10-14 22:06:59 +0900

    :art: DirecyX.h のcode追加

--- ActionPractice/ActionPractice/DirectX.h
+++ ActionPractice/ActionPractice/DirectX.h
@@ -1,1 +1,24 @@
-﻿
+﻿#ifndef DIRECTX_H
+#define DIRECTX_H
+
+#include<windows.h>
+#include<d3dx9.h>
+#include<tchar.h>
+#include<dinput.h>
+
+class DirectX
+{
+public:
+
+	LPDIRECT3D9 pDirect3d;
+	LPDIRECT3DDEVICE9 pDevice;
+	LPDIRECTINPUT8 pDinput;
+	LPDIRECTINPUTDEVICE8 pkey;
+	LPD3DXMESH pMesh;
+	D3DMATERIAL9* pMeshMaterials;
+	LPDIRECT3DTEXTURE9* pMeshTextures;
+
+	DWORD dwNumMaterials = 0;
+};
+
+#endif// !1
